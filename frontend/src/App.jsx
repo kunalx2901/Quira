c83f87456c6ffc2622cc5808e1c91a6553afb138
Kunal Sharma <kunal.sharma.29.06.2003@gmail.com>
2025-06-07 23:46:31 +0530

    pages_added'
    '

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 
 import React from 'react'
+import { Routes, Route} from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ChatPage from './pages/ChatPage';
+import CallPage from './pages/CallPage';
+import NotificationPage from './pages/NotificationPage';
+import { toast , Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div className="text-5xl h-screen " data-theme="bumblebee">
-      <button class="btn btn-primary">One</button>
-      <button class="btn btn-secondary">Two</button>
-      <button class="btn btn-accent btn-outline">Three</button> 
+    <div className="text-2xl h-screen " data-theme="bumblebee">
+      <button onClick={()=>{toast.success("hello world !")}}>Create a toast</button>
+      <Routes>
+        <Route path="/" element={HomePage}></Route>
+        <Route path="/login" element={Login}></Route>
+        <Route path="/signup" element={SignUp}></Route>
+        <Route path="/chat" element={ChatPage}></Route>
+        <Route path="/call" element={CallPage}></Route>
+        <Route path="/notification" element={NotificationPage}></Route>
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
