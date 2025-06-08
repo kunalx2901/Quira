973da72e362ff721fc53e4b14b3633ab3ba30e0a
Kunal Sharma <kunal.sharma.29.06.2003@gmail.com>
2025-06-08 23:27:20 +0530

    cors_error

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 
@@ -11,6 +12,11 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
