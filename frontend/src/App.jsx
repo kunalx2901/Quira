12d3b2d99a989da92985a6dddd2cae5256b8fae2
Kunal Sharma <kunal.sharma.29.06.2003@gmail.com>
2025-06-08 23:55:35 +0530

    adding_tanstackQuery

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,34 @@ import ChatPage from './pages/ChatPage';
 import CallPage from './pages/CallPage';
 import NotificationPage from './pages/NotificationPage';
 import { toast , Toaster } from 'react-hot-toast';
+import {useQuery }from "@tanstack/react-query"
+import axios from "axios";
+import { axiosInstance } from './lib/axios.js';
 
 const App = () => {
+
+  const {data, isLoading, error}= useQuery({
+        queryKey:['todos'],
+        queryFn: async()=>{
+          const res = await axiosInstance.get("/auth/me")
+         return res.data;
+        },
+        retry:false
+      })
+
+      console.log({ data })
+      console.log({ isLoading })
+      console.log({ error })
+
   return (
     <div className="text-2xl h-screen " data-theme="bumblebee">
-      <button onClick={()=>{toast.success("hello world !")}}>Create a toast</button>
       <Routes>
-        <Route path="/" element={HomePage}></Route>
-        <Route path="/login" element={Login}></Route>
-        <Route path="/signup" element={SignUp}></Route>
-        <Route path="/chat" element={ChatPage}></Route>
-        <Route path="/call" element={CallPage}></Route>
-        <Route path="/notification" element={NotificationPage}></Route>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/chat" element={<ChatPage/>}></Route>
+        <Route path="/call" element={<CallPage/>}></Route>
+        <Route path="/notification" element={<NotificationPage/>}></Route>
       </Routes>
       <Toaster/>
     </div>
